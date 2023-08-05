a5ed5e8e21314bc25ec3bf36f4c5810c00cd26cf
saket <saketthakur4797@gmail.com>
2023-08-05 09:11:40 +0530

    removed the comments and added a loop

--- scrape.py
+++ scrape.py
@@ -9,24 +9,71 @@ import random
 import os
 import time
 import re
-
-# Additional imports
 from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+
 
 # Your existing setup
 options = webdriver.ChromeOptions()
 options.add_argument("--user-data-dir=C:\\Temp\\ChromeProfile")
 driver = webdriver.Chrome(options=options)
 
+
+def get_comments(comments_html):
+    # Parse the HTML using BeautifulSoup
+    soup = BeautifulSoup(comments_html, 'html.parser')
+
+    # Extract comments along with usernames and their links
+    comments_data = []
+    for li in soup.find_all('li'):
+        # Check for a username and its associated link within the comment
+        user_link_element = li.find('a')
+        if user_link_element:
+            user_link = user_link_element.get('href')
+            # Extract the username from the user link (assuming the format is "/username/")
+            username = user_link.strip('/')
+            # Extract the comment text
+            comment_text = li.get_text().replace(username, '', 1).strip()
+            comments_data.append({
+                'user-id': username,
+                'User_Profile_link': f"https://www.instagram.com{user_link}",
+                'comment': comment_text
+            })
+    
+    # Get the JavaScript code of the page
+
+
+    # Further refine the extraction process to capture the timestamp, likes, and clean up the comment text
+    for comment in comments_data:
+        # Extract the timestamp using regex
+        timestamp_match = re.search(r'(\d+[smh])', comment['comment'])
+        if timestamp_match:
+            comment['time'] = timestamp_match.group(1)
+            # Remove the timestamp from the comment text
+            comment['comment'] = comment['comment'].replace(comment['time'], '', 1).strip()
+
+        # Extract the likes count using regex
+        likes_match = re.search(r'(\d+) like', comment['comment'])
+        if likes_match:
+            comment['likes'] = int(likes_match.group(1))
+            # Remove the likes count from the comment text
+            comment['comment'] = comment['comment'].replace(f"{comment['likes']} like", '', 1).strip()
+        elif 'like' in comment['comment']:
+            comment['likes'] = 1
+            # Remove the "like" text from the comment
+            comment['comment'] = comment['comment'].replace('like', '', 1).strip()
+        else:
+            comment['likes'] = 0
+
+        # Remove any metadata from the comment text
+        comment['comment'] = comment['comment'].replace('ReplyComment OptionsLike', '', 1).strip()
+    return comments_data
+
+
+
+
 # Navigate to the Instagram page
 driver.get('https://www.instagram.com/hdfcbank/')
-
-# test
-# driver.get('https://www.instagram.com/p/CvKmgR1Ij4f/')
-# test
-
-# Wait for the posts to load
-# time.sleep(4)
 
 # Wait for the div with class _aagw to be clickable
 element = WebDriverWait(driver, 10).until(
@@ -41,7 +88,7 @@ ul_element = driver.find_element(By.CSS_SELECTOR, "ul._a9z6._a9za")
 # Get the inner HTML of the ul element
 html_content = ul_element.get_attribute('outerHTML')
 
-print(html_content)
+# print(html_content)
 
 
 # Scroll to the bottom of the comments section
@@ -49,116 +96,59 @@ actions = ActionChains(driver)
 actions.move_to_element(ul_element).perform()
 
 
-# # Wait for the div elements with class _aagw to be available
-# elements = WebDriverWait(driver, 10).until(
-#     EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div._aagw"))
-# )
-
-# # Click on the 4th element
-# # Ensure that there are at least 4 posts before trying to click on the 4th
-# if len(elements) >= 4:
-#     elements[3].click()
-# else:
-#     print("Less than 4 posts available.")
-
-# # Rest of your code remains the same
-
-# Find the ul element with classes _a9z6 _a9za
-ul_element = driver.find_element(By.CSS_SELECTOR, "ul._a9z6._a9za")
-
-# Get the inner HTML of the ul element
-html_content = ul_element.get_attribute('outerHTML')
-print(html_content)
-
 # Scroll to the bottom of the comments section
 actions = ActionChains(driver)
 actions.move_to_element(ul_element).perform()
 
+
+# comments and other details
+posts = []
 
 # Wait a bit for potential loading animations or transitions
 time.sleep(2)
 
-
-# Importing Keys
-from selenium.webdriver.common.keys import Keys
-
-# ... [Your existing code]
-
-# Send the "End" key to the page to scroll to the bottom
-# driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
-# time.sleep(2)
-
-# Send the "End" key to the body of the webpage to scroll to the bottom
-# driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
-
-# Identify the comments container/modal. This might require updating the selector to target the correct element.
-
 for i in range(6):
     comments_container = driver.find_element(By.CSS_SELECTOR, "ul._a9z6._a9za")
+    button = True
+    while button:
+        button = False
+        # Use JavaScript to scroll to the bottom of the comments container/modal
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight;", comments_container)
+        print('scrolling to the bottom')
+        time.sleep(1)
 
-    # # Send the "End" key to the comments container to scroll to the bottom of the comments
-    # comments_container.send_keys(Keys.END)
+        # Check if the "Load more comments" button with the specific attributes is present
+        try:
+            load_more_button = driver.find_element(By.CSS_SELECTOR, 'button._abl- div._abm0 svg[aria-label="Load more comments"]')
+            load_more_button.click()
+            print('hurray clicked on the button 😎😎')
+            button = True
+            # Optionally add a sleep after clicking the button to ensure the new comments are loaded
+            time.sleep(2)
 
-    # Use JavaScript to scroll to the bottom of the comments container/modal
-    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight;", comments_container)
+        except NoSuchElementException:
+            # If the button is not found, it means all comments are already loaded or the specific button is not present
+            pass
 
-    # Wait a bit to ensure that any dynamically loaded content has had time to appear
-    # time.sleep(4)
+    # Find the ul element with classes _a9z6 _a9za
+    ul_element = driver.find_element(By.CSS_SELECTOR, "ul._a9z6._a9za")
 
-    print('trying')
-
-
-    # Wait a bit to ensure the page has scrolled to the end and any potential loading animations complete
-    time.sleep(2)
-
-    # Check if the "Load more comments" button with the specific attributes is present
-    try:
-        load_more_button = driver.find_element(By.CSS_SELECTOR, 'button._abl- div._abm0 svg[aria-label="Load more comments"]')
-        load_more_button.click()
-        print('hurray clicked on the button 😎😎')
-        # Optionally add a sleep after clicking the button to ensure the new comments are loaded
-        time.sleep(2)
-    except NoSuchElementException:
-        # If the button is not found, it means all comments are already loaded or the specific button is not present
-        pass
-        
-    print('done')
+    # Get the inner HTML of the ul element
+    html_content = ul_element.get_attribute('outerHTML')
+    comments = get_comments(html_content)
+    posts.append(comments)
+    break
+    # click on the next button
     # Find the specific button inside the div with classes "_aaqg _aaqh" and click on it
     specific_button = driver.find_element(By.CSS_SELECTOR, "div._aaqg._aaqh > button._abl-")
     specific_button.click()
-    # Find the button with class "_abl-" and click on it
-    # load_more_comments_button = driver.find_element(By.CSS_SELECTOR, "button._abl-")
-    # load_more_comments_button.click()
 
-    # # Check if a button with class "_abl-" is present
-    # try:
-    #     load_more_button = driver.find_element(By.CSS_SELECTOR, "button._abl-")
-    #     if load_more_button:
-    #         load_more_button.click()
-    #         # Optionally add a sleep after clicking the button to ensure the new comments are loaded
-    #         time.sleep(2)
-    # except NoSuchElementException:
-    #     # If the button is not found, it means all comments are already loaded or there's no more button
-    #     pass
+import json
 
-    # ... [The rest of your code]
-
-
-
-
-
-    # # Check if a button with class "_abl-" is present
-    # try:
-    #     load_more_button = driver.find_element(By.CSS_SELECTOR, "button._abl-")
-    #     if load_more_button:
-    #         load_more_button.click()
-    #         # Optionally add a sleep after clicking the button to ensure the new comments are loaded
-    #         time.sleep(2)
-    # except NoSuchElementException:
-    #     # If the button is not found, it means all comments are already loaded or there's no more button
-    #     pass
-
-    # ... [The rest of your code]
+# Save the posts list to a text file
+with open('posts.txt', 'w') as file:
+    file.write(json.dumps(posts, indent=4))
 
 
 time.sleep(1000)
+
